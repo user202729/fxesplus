32c8fc634dacfc42d7bdecb35f501fd29ee07092
user202729 <25191436+user202729@users.noreply.github.com>
2019-01-07 23:38:26 +0700

    Move common Lua code to emulator/lua-common.lua

--- run-emu.sh
+++ run-emu.sh
@@ -3,5 +3,6 @@ cd "$(dirname $0)"
 if [ ! -f CasioEmu/models/fx570esplus/rom.bin ]; then
 	cp -l rom.bin CasioEmu/models/fx570esplus/rom.bin
 fi
-CasioEmu/emulator/bin/casioemu CasioEmu/models/fx570esplus/
-
+CasioEmu/emulator/bin/casioemu \
+	model=CasioEmu/models/fx570esplus/ \
+	script=CasioEmu/emulator/lua-common.lua
