df0869619b2907521a1f52e1c17a7e61a04ee29b
user202729 <25191436+user202729@users.noreply.github.com>
2019-01-07 22:35:24 +0700

    Disassemble should use luajit is possible
    With the given optimizations, on my machine, lua takes 1.5s while
    luajit takes 1s. So luajit is faster.

--- disassemble.sh
+++ disassemble.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 cd "$(dirname $0)"
-lua CasioEmu/disassembler/disassembler.lua \
+
+if which luajit; then
+	cmd=luajit
+else
+	cmd=lua
+fi
+
+$cmd CasioEmu/disassembler/disassembler.lua \
 	input='CasioEmu/models/fx570esplus/rom.bin' \
 	output='./fx_570es+_disas.txt' \
 	entry='1,2,4,5,6,7,8,9,10,11,12,13,14,15,16' \
 	strict='true' addresses='true' rom_window='0x8000' \
 	entry_addresses_file='./entry_addresses.txt' \
 	names='./570es+_names.txt' \
-	$@
+	"$@"
