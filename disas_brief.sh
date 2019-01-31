f90467ae00d1e34a04b37633c431be4f0bab0f02
user202729 <25191436+user202729@users.noreply.github.com>
2019-01-31 10:21:40 +0700

    Fix: make disas_brief work from any directory
    Also delete lua-helper.lua.

--- disas_brief.sh
+++ disas_brief.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+basedir="$(dirname "$0")"
+
 if [[ $# == 0 ]]; then
-	input="82esp/emu_rom.bin"
-	output="82esp/emu_brief_disas.txt"
+	input="$basedir/82esp/emu_rom.bin"
+	output="$basedir/82esp/_b_emu_disas.txt"
 elif [[ $# == 2 ]]; then
 	input="$1"
 	output="$2"
@@ -11,4 +13,4 @@ else
 	exit
 fi
 
-disas/bin/u8-disas-brief "$input" 0 $(wc -c < "$input") "$output"
+"$basedir/disas/bin/u8-disas-brief" "$input" 0 $(wc -c < "$input") "$output"
