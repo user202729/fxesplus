c70f1808fceccc626b4c3a2da2e50597d5e94b83
user202729 <25191436+user202729@users.noreply.github.com>
2019-03-04 21:02:12 +0700

    File reorganization: vimrc, .gitignore, emu_history
    * Each model now has its own `.emu_history` file.
    * `.gitignore` avoids unnecessary glob.

--- 570esp/emu.sh
+++ 570esp/emu.sh
@@ -6,5 +6,5 @@ fi
 CasioEmu/emulator/bin/casioemu \
 	model=CasioEmu/models/fx570esplus/ \
 	script=570esp/lua-emu-init.lua \
-	history=emu-history \
+	history=570esp/.emu_history \
 	"$@"
---
+++ 82espa/.vimrc
@@ -0,0 +1,3 @@
+" gd: Go to definitions (currently only work with functions)
+nnoremap gd yiw/^<c-r>":<cr>
+nnoremap <leader>d :up<bar>!./r_disas.sh<cr>l<cr>
