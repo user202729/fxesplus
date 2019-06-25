8ea7ea43f1ac80db14b4145e78fa6a06d18578d4
user202729 <25191436+user202729@users.noreply.github.com>
2019-06-25 16:31:05 +0700

    Update emulator&disassembler, turn on word_commands by default

--- 570esp/r_disas.sh
+++ 570esp/r_disas.sh
@@ -11,6 +11,6 @@ $cmd CasioEmu/disassembler/disassembler.lua \
 	input='CasioEmu/models/fx570esplus/rom.bin' \
 	output='570esp/disas.txt' \
 	entry='1,2,4,5,6,7,8,9,10,11,12,13,14,15,16' \
-	strict='true' addresses='true' rom_window='0x8000' \
+	strict='true' addresses='true' word_commands='true' rom_window='0x8000' \
 	names='570esp/labels' \
 	"$@"
