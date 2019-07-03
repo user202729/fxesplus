948006010043d492a13a17dbe20f6cef61da557d
user202729 <25191436+user202729@users.noreply.github.com>
2019-07-03 18:50:40 +0700

    Allow revbin to work with fractional number of bits

--- revbin.py
+++ revbin.py
@@ -5,5 +5,9 @@ Reverse raw binary dump.
 
 import sys
 data = ''.join(x for x in sys.stdin.read() if x in '01')
-assert len(data)%8==0
-sys.stdout.buffer.write(int(data,2).to_bytes(len(data)//8,'big'))
+extra = len(data)%8
+if extra:
+	sys.stderr.write(f'Last {extra} bits truncated\n')
+	data = data[:-extra]
+if data:
+	sys.stdout.buffer.write(int(data,2).to_bytes(len(data)//8,'big'))
