9b2069ab2c95225fef67739a33e142ebfb7cab8b
user202729 <25191436+user202729@users.noreply.github.com>
2019-10-07 17:28:10 +0700

    Add initial labels file for 580vnx, make r_disas import labels_sfr

--- 580vnx/r_disas.sh
+++ 580vnx/r_disas.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 cd "$(dirname $0)/.."
 
 if which luajit; then
@@ -12,5 +12,5 @@ $cmd CasioEmu/disassembler/disassembler.lua \
 	output='580vnx/disas.txt' \
 	entry='1,2,4,5,6,7,8,9,10,11,12,13,14,15,16' \
 	strict='true' addresses='true' word_commands='true' rom_window='0xd000' \
+	names=<(cat '580vnx/labels' 'labels_sfr') \
 	"$@"
-	# names='580vnx/labels' \
--- 82espa/r_disas.sh
+++ 82espa/r_disas.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 cd "$(dirname $0)/.."
 
 if which luajit; then
@@ -12,5 +12,5 @@ $cmd CasioEmu/disassembler/disassembler.lua \
 	output='82espa/disas.txt' \
 	entry='1,2,4,5,6,7,8,9,10,11,12,13,14,15,16' \
 	strict='true' addresses='true' word_commands='true' rom_window='0x8000' \
-	names='82espa/labels' \
+	names=<(cat '82espa/labels' 'labels_sfr') \
 	"$@"
