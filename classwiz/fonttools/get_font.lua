3ed65632594619db8144e5269055fa885d588d9c
user202729 <25191436+user202729@users.noreply.github.com>
2020-06-03 19:27:47 +0700

    Better documentation for classwiz/fonttools/get_font.lua

--- classwiz/fonttools/get_font.lua
+++ classwiz/fonttools/get_font.lua
@@ -2,24 +2,33 @@
 -- Usage:
 --   Set break addresses (depends on the calculator model)
 --   Call main function.
---   Output is written to `out`.
+--   Output is written to (outfilepath).
 --   Output file format: text file with '#' = on and ' ' = off.
 -- Use to_png.py to convert output file to png.
+
+--line_print_after_push_lr=0x09132 -- 580 emulator
+line_print_after_push_lr=0x083D8 -- 991CNX emulator
+current_screen_buffer=0xD139
+screen_buffer_location=0xe3d4
+start_sp=0xef00
+outfilepath='/tmp/out'
 
 function a(base)
 	printf('"' ..
 		(base>=0x100 and '%04X' or '  %02X')
 		.. ':",', base)
 	b()
-	cpu.csr=0x0 cpu.pc=0x9132 -- after push lr of line_print
+	cpu.csr=line_print_after_push_lr>>16
+	cpu.pc=line_print_after_push_lr&0xffff
+	data[current_screen_buffer]=0
 
 	for row=0,16 do
 		for col=0,23 do
-			data[0xe3d4+24*row+col]=0
+			data[screen_buffer_location+24*row+col]=0
 		end
 	end
 
-	local sp=0xef00
+	local sp=start_sp
 	cpu.sp=sp
 
 	local st=0xef20
@@ -44,19 +53,19 @@ function a(base)
 	cpu.r2=st&0xff
 	cpu.r3=st>>8
 
-	while cpu.csr~=0 or cpu.pc~=0x91a6 do
-		--local old=cpu.csr<<16|cpu.pc
+	while cpu.sp<=start_sp do
+		local old=cpu.csr<<16|cpu.pc
 		emu:tick()
-		--local new=cpu.csr<<16|cpu.pc
-		--if old==new then
-		--	printf('old=%x,new=%x,same',old,new)
-		--	return
-		--end
+		local new=cpu.csr<<16|cpu.pc
+		if old==new then
+			printf('old=%x,new=%x,same',old,new)
+			return
+		end
 	end
 
 	for row=0,16 do
 		for col=0,23 do
-			local bt=data[0xe3d4+24*row+col]
+			local bt=data[screen_buffer_location+24*row+col]
 			for bit=7,0,-1 do
 				fi:write(((bt>>bit)&1)==1 and '#' or ' ')
 			end
@@ -66,6 +75,7 @@ function a(base)
 	fi:flush()
 end
 
+-- Assumes that there's no existing breakpoint.
 function waitret(cb)
 	local z=function()
 		del(1)
@@ -78,8 +88,10 @@ function waitret(cb)
 end
 
 function main()
+	-- because it's very likely that the calculator is blocking while this command is executed
+	-- it's necessary to execute `a` asynchronously when it continues.
 	waitret(function()
-		fi = io.open('out','w')
+		fi = io.open(outfilepath,'w')
 		for i=0x00,0xef,0x10 do a(i) end
 		for i=0xf000,0xffff,0x10 do
 			if i&0xff~=0xf0 then
